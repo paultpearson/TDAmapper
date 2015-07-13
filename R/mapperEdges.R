3298c15f688438ee790cefbd7c3062a4285982f0
Paul Pearson <paultpearson@gmail.com>
2015-07-13 16:52:39 -0400

    Add documentation

--- R/mapperEdges.R
+++ R/mapperEdges.R
@@ -1,3 +1,43 @@
+#' mapperEdges function
+#'
+#' The input to this function is a TDAmapper class object and the output
+#' is a data frame of edges that can be used as input to the networkD3 
+#' plot utility.
+#'
+#' @param m An object of class TDAmapper that is the output of the mapper function.
+#'
+#' @return A data frame describing the edges in the graph of the mapper output.
+#'
+#' @author Paul Pearson, \email{pearsonp@@hope.edu}
+#' @references \url{https://github.com/paultpearson/TDAmapper}
+#' @seealso \code{\link{mapperVertices}}
+#' @keywords mapperEdges
+#'
+#' @examples
+#' \dontrun{
+#' X <- data.frame( x=2*cos(0.5*(1:100)), y=sin(1:100) )
+#' f <- X$y
+#' m1 <- mapper(
+#'        distance_matrix = dist(X),
+#'        filter_values = f,
+#'        num_intervals = 10,
+#'        percent_overlap = 50,
+#'        num_bins_when_clustering = 10)
+#'        
+#' pt_labels <- 1:length(f)
+#' vertices <- mapperVertices(m1, pt_labels)
+#' edges <- mapperEdges(m1)
+#' 
+#' # interactive plot
+#' forceNetwork(Nodes = nodes, Links = links, 
+#'             Source = "Linksource", Target = "Linktarget",
+#'             Value = "Linkvalue", NodeID = "Nodename",
+#'             Group = "Nodegroup", opacity = 0.8, 
+#'             linkDistance = 10, charge = -400)
+#' }
+#' @export
+#'
+
 mapperEdges <- function(m) {
     linksource <- c()
     linktarget <- c()
--- R/mapperVertices.R
+++ R/mapperVertices.R
@@ -1,3 +1,47 @@
+#' mapperVertices function
+#'
+#' The input to this function is a TDAmapper class object and the output
+#' is a data frame of vertices that can be used as input to the networkD3 
+#' plot utility.
+#'
+#' @param m An object of class TDAmapper that is the output of the mapper 
+#' function.
+#'
+#' @return A data frame describing the vertices in the graph of the mapper 
+#' output and the point labels that will be displayed when the mouse 
+#' hovers over a vertex in the graph.
+#'
+#' @author Paul Pearson, \email{pearsonp@@hope.edu}
+#' @references \url{https://github.com/paultpearson/TDAmapper}
+#' @seealso \code{\link{mapperEdges}}
+#' @keywords mapperVertices
+#'
+#' @examples
+#' \dontrun{
+#' X <- data.frame( x=2*cos(0.5*(1:100)), y=sin(1:100) )
+#' f <- X$y
+#' m1 <- mapper(
+#'        distance_matrix = dist(X),
+#'        filter_values = f,
+#'        num_intervals = 10,
+#'        percent_overlap = 50,
+#'        num_bins_when_clustering = 10)
+#'        
+#' pt_labels <- 1:length(f)
+#' vertices <- mapperVertices(m1, pt_labels)
+#' edges <- mapperEdges(m1)
+#' 
+#' # interactive plot
+#' forceNetwork(Nodes = nodes, Links = links, 
+#'             Source = "Linksource", Target = "Linktarget",
+#'             Value = "Linkvalue", NodeID = "Nodename",
+#'             Group = "Nodegroup", opacity = 0.8, 
+#'             linkDistance = 10, charge = -400)
+#' }
+#' @export
+#'
+
+
 mapperVertices <- function(m, pt_labels) {
     
     # Hovering over vertices gives the point labels:
